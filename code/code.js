64eea839f325f4d602e3a950218ed0c4c67dad31
Becca <beccahagg5@gmail.com>
2019-12-20 23:03:20 +0000

    done

--- code/code.js
+++ code/code.js
@@ -1,35 +1,17 @@
 var sketchProc = function (processingInstance) {
     with (processingInstance) {
-        var offCenterX =120;
-        var centerX = 200;
-        var centerY = 200;
-        var offCenterY = 60;
-        var diameter = 50;
-        var drawDiagram = function() {
-            background(255, 255, 255);
-            fill(0, 0, 0);
-                //noFill();
-            stroke(0, 0, 0);
-            
-            ellipse(centerX - offCenterX,centerY + offCenterY,diameter,diameter);
-            ellipse(centerX + offCenterX,centerY + offCenterY,diameter,diameter);
-            ellipse(centerX,centerY + offCenterY,diameter,diameter);
-            
-            ellipse(centerX,centerY - offCenterY,diameter,diameter);
-            
-            line(centerX, centerY - offCenterY + diameter/2, centerX, centerY + offCenterY - diameter/2);
-            
-            line(centerX, centerY - offCenterY + diameter/2, centerX - offCenterX, centerY + offCenterY - diameter/2);
-            
-            line(centerX, centerY - offCenterY + diameter/2, centerX + offCenterX, centerY + offCenterY - diameter/2);   
-        }
+        // !! DEFINE FUNCTIONS HERE !!
+        // EXAMPLE: var drawDiagram = function() {...}
+        
         size(400, 400);
         frameRate(30);
-        drawDiagram();
+        // !! PUT THE FUNCTION EXECUTE CALLS HERE !!
+        // EXAMPLE: drawDiagram();
     }
 };
 
 // Get the canvas that Processing-js will use
-var canvas = document.getElementById("diagramCanvas");
-// Pass the function sketchProc (defined in myCode.js) to Processing's constructor.
+// !! PUT THE CANVAS_ID HERE !!
+var canvas = document.getElementById("CANVAS_ID");
+// Pass the function sketchProc (defined in code.js) to Processing's constructor.
 var processingInstance = new Processing(canvas, sketchProc);
